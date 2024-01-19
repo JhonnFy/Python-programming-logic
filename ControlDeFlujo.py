a2639e74729b8d6fe35a4e6f56e5b314156e8baa
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-01-19 00:51:11 -0500

    ControlesDeFlujo

---
+++ ControlDeFlujo.py
@@ -0,0 +1,18 @@
+
+a = 0
+if a > 0:
+    print('El numero es Positivo')
+elif a == 0:
+    print('El numero es neutro')
+else:
+    print('El numero es Negativo')
+    
+while a <= 5:
+    print(a)
+    a += 1
+    
+numeros = [1,2,3,4,5]
+
+for n in numeros:
+    print(n)
+    
